599883383b8d86d3b241270417611a596f63ff05
de1mossss <pashhin2001@mail.ru>
2023-02-27 14:01:18 +0700

    Added executable parameter

--- src/main/java/com/testspring/webhook/controller/WebhookController.java
+++ src/main/java/com/testspring/webhook/controller/WebhookController.java
@@ -2,10 +2,8 @@ package com.testspring.webhook.controller;
 
 import com.testspring.webhook.service.WebhookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,6 +17,11 @@ public class WebhookController {
         Map<String, Object> responseBody = new HashMap<>();
         responseBody = service.getRequestData();
         if(requestBody != null){
+            if(requestBody.containsKey("execute")){
+                Map<String, Object> executeBody = (Map<String, Object>) requestBody.get("execute");
+                executeBody.forEach((key, value) -> executeBody.put(key, service.getMethod(value.toString())));
+                requestBody.put("execute", executeBody);
+            }
             responseBody.put("content", requestBody);
         }
         return responseBody;
